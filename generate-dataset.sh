5eb2cada9dcdf125e513e85d32bd8d9675867522
Michał Budziak <michal.mariusz.b@gmail.com>
2017-10-30 16:49:35 +0100

    Fix generating datasets

--- generate-dataset.sh
+++ generate-dataset.sh
@@ -4,5 +4,5 @@ dd if=/dev/urandom of=./storage/dataset-512b bs=512 count=1
 dd if=/dev/urandom of=./storage/dataset-512kB bs=512kB count=1
 dd if=/dev/urandom of=./storage/dataset-4MB bs=4M count=1
 dd if=/dev/urandom of=./storage/dataset-32MB bs=32M count=1
-dd if=/dev/urandom of=./storage/dataset-64MB bs=64M count=2
-dd if=/dev/urandom of=./storage/dataset-128MB bs=128M count=4
+dd if=/dev/urandom of=./storage/dataset-64MB bs=32M count=2
+dd if=/dev/urandom of=./storage/dataset-128MB bs=32M count=4
