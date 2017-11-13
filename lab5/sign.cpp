cd48f2e8b4e36398e61799f2d81eb9f386f04e6a
Michał Budziak <michal.mariusz.b@gmail.com>
2017-11-13 16:37:59 +0100

    Refactor vol2

--- lab5/sign.cpp
+++ lab5/sign.cpp
@@ -6,103 +6,95 @@
 #include <openssl/md5.h>
 
 
-unsigned char* md5sum(char* inputFileName){
-  FILE* inputFile;
-  unsigned char inputVector[16];
-  unsigned char* md5Vector= (unsigned char*)malloc(16*sizeof(unsigned char));
-  if ((inputFile=fopen(inputFileName, "rb"))==NULL){
-    fprintf(stderr, "Otwarcie pliku %s sie nie powiodlo\n", inputFileName);
-    exit(1);
-  }
+unsigned char *md5sum(char *inputFileName) {
+    FILE *inputFile;
+    unsigned char inputVector[16];
+    unsigned char *md5Vector = (unsigned char *) malloc(16 * sizeof(unsigned char));
+    if ((inputFile = fopen(inputFileName, "rb")) == NULL) {
+        fprintf(stderr, "Otwarcie pliku %s sie nie powiodlo\n", inputFileName);
+        exit(1);
+    }
 
-  /* Czastka hasha */
-  MD5_CTX hashChunk;
-  MD5_Init(&hashChunk);
+    MD5_CTX hashChunk;
+    MD5_Init(&hashChunk);
 
-  /* Obliczanie hasha */
-  size_t bytesRead;
-  while(1){
-    bytesRead=fread(inputVector, sizeof(char), sizeof(inputVector), inputFile);
-    if (bytesRead==0) break;
-    MD5_Update(&hashChunk, inputVector, bytesRead);
-  }
-  MD5_Final(md5Vector, &hashChunk);  
+    size_t bytesRead;
+    while (1) {
+        bytesRead = fread(inputVector, sizeof(char), sizeof(inputVector), inputFile);
+        if (bytesRead == 0) break;
+        MD5_Update(&hashChunk, inputVector, bytesRead);
+    }
+    MD5_Final(md5Vector, &hashChunk);
 
-  return md5Vector;
+    return md5Vector;
 }
 
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage:\n fileToSign privateKeyPath\n");
+        return 1;
+    }
 
-int rsaSign(){ //return 0 - sukces 1 - porazka
+    char *keyFileName = argv[2];
+    char *toSignFileName = argv[1];
+    char *signFileName;
+    FILE *keyFile;
+    FILE *signFile;
+    RSA *rsaPrivateKey;
+    unsigned char *md5Vector;
 
-}
-int main(int argc, char* argv[]){   
-	if (argc!=3){
-		fprintf(stderr, "Uzycie programu:\n nazwaPlikuDoPodpisania nazwaPlikuKluczaPrywatnego\n");
-		return 1;
-	}
-	char* keyFileName = argv[2]; 
-	char* toSignFileName  = argv[1];
-	char* signFileName;
-	FILE* keyFile;
-	FILE* signFile;
-	RSA* rsaPrivateKey;
-	unsigned char* md5Vector;
-	/* do dekodowania klucza */
-	long keySize;
-	unsigned char* buffer;
-	unsigned char* buffer2;
+    long keySize;
+    unsigned char *buffer;
+    unsigned char *buffer2;
 
-	/* tworzenie nazwy pliku z podpisem nazwaPlikuDoPodpisania.sig */
-	signFileName=(char*)malloc(sizeof(toSignFileName)+4*sizeof(char));
-	strcpy(signFileName,toSignFileName);
-	strcat(signFileName,".sig");
-	
-	/* wczytywanie klucza prywatnego */
-	rsaPrivateKey=RSA_new();
-	if ((keyFile=fopen(keyFileName,"rb"))==NULL){
-		fprintf(stderr, "Nie udalo sie otworzyc pliku do odczytu klucza prywatnego\n");
-		return 1;
-	}
-	
-	fseek(keyFile,0,SEEK_END);
-	keySize=ftell(keyFile);
-	rewind(keyFile);
-	
-	buffer=(unsigned char*)malloc(keySize*sizeof(unsigned char));
-	buffer2=buffer; //jesli probuje bezposrednio to wywala Naruszenie Ochrony Pamieci
-	fread(buffer,sizeof(unsigned char),keySize,keyFile);
-	d2i_RSAPrivateKey(&rsaPrivateKey,(const unsigned char**)&buffer2,keySize); //dekodowanie klucza prywatnego z PKCS do RSA d2i_RSAPrivateKey(**kluczRSA, **bufforZPKCS, dlguoscKluczaPKCS)
-	fclose(keyFile);
-	free(buffer);
-	
-	if (rsaPrivateKey==NULL){
-		fprintf(stderr, "Nie udalo sie wczytac klucza prywatnego\n");
-		return 1;
-	}
-	
-	md5Vector=md5sum(toSignFileName);
-	
-	buffer=(unsigned char*)malloc(RSA_size(rsaPrivateKey));//miejsce na podpis
-	unsigned int signLength; //dlugosc podpisu
-	if ((RSA_sign(NID_md5,md5Vector,sizeof(md5Vector),buffer,&signLength,rsaPrivateKey))==0){ //podpisywanie RSA_sign(tryb NID_nazwafunkcjihashujacej, *tablicaZHashem, dlugoscTablicyZHashem, *miejsceNaPodpis, *miejsceNaDlugoscPodpisu, *kluczRSA)
-		fprintf(stderr, "Nie udalo sie podpisac pliku\n");
-		return 1;
-	}
-	
-	if ((signFile=fopen(signFileName,"wb"))==NULL){
-		fprintf(stderr, "Nie udalo sie otworzyc pliku do zapisu podpisu\n");
-		return 1;
-	}
-	fwrite(buffer,sizeof(unsigned char),signLength,signFile);
-	
-	fflush(signFile);
-	fclose(signFile);
-	
-	free(buffer);
-	free(md5Vector);
-	free(signFileName);
-	RSA_free(rsaPrivateKey);
-	
-	return 0;
+    signFileName = (char *) malloc(sizeof(toSignFileName) + 4 * sizeof(char));
+    strcpy(signFileName, toSignFileName);
+    strcat(signFileName, ".sig");
 
+    rsaPrivateKey = RSA_new();
+    if ((keyFile = fopen(keyFileName, "rb")) == NULL) {
+        fprintf(stderr, "Open rb priv key error\n");
+        return 1;
+    }
+
+    fseek(keyFile, 0, SEEK_END);
+    keySize = ftell(keyFile);
+    rewind(keyFile);
+
+    buffer = (unsigned char *) malloc(keySize * sizeof(unsigned char));
+    buffer2 = buffer;
+    fread(buffer, sizeof(unsigned char), keySize, keyFile);
+    d2i_RSAPrivateKey(&rsaPrivateKey, (const unsigned char **) &buffer2, keySize);
+    fclose(keyFile);
+    free(buffer);
+
+    if (rsaPrivateKey == NULL) {
+        fprintf(stderr, "Read priv key error\n");
+        return 1;
+    }
+
+    md5Vector = md5sum(toSignFileName);
+
+    buffer = (unsigned char *) malloc(RSA_size(rsaPrivateKey));//miejsce na podpis
+    unsigned int signLength; //dlugosc podpisu
+    if ((RSA_sign(NID_md5, md5Vector, sizeof(md5Vector), buffer, &signLength, rsaPrivateKey)) == 0) {
+        fprintf(stderr, "Problem with signing a file\n");
+        return 1;
+    }
+
+    if ((signFile = fopen(signFileName, "wb")) == NULL) {
+        fprintf(stderr, "Open file to save sign error\n");
+        return 1;
+    }
+    fwrite(buffer, sizeof(unsigned char), signLength, signFile);
+
+    fflush(signFile);
+    fclose(signFile);
+
+    free(buffer);
+    free(md5Vector);
+    free(signFileName);
+    RSA_free(rsaPrivateKey);
+
+    return 0;
 }
--- lab5/verify.cpp
+++ lab5/verify.cpp
@@ -5,102 +5,99 @@
 #include <openssl/engine.h>
 #include <openssl/md5.h>
 
+unsigned char *md5sum(char *inputFileName) {
+    FILE *inputFile;
+    unsigned char inputVector[16];
+    unsigned char *md5Vector = (unsigned char *) malloc(16 * sizeof(unsigned char));
+    if ((inputFile = fopen(inputFileName, "rb")) == NULL) {
+        fprintf(stderr, "Otwarcie pliku %s sie nie powiodlo\n", inputFileName);
+        exit(1);
+    }
 
+    /* Czastka hasha */
+    MD5_CTX hashChunk;
+    MD5_Init(&hashChunk);
 
-unsigned char* md5sum(char* inputFileName){
-  FILE* inputFile;
-  unsigned char inputVector[16];
-  unsigned char* md5Vector= (unsigned char*)malloc(16*sizeof(unsigned char));
-  if ((inputFile=fopen(inputFileName, "rb"))==NULL){
-    fprintf(stderr, "Otwarcie pliku %s sie nie powiodlo\n", inputFileName);
-    exit(1);
-  }
+    /* Obliczanie hasha */
+    size_t bytesRead;
+    while (1) {
+        bytesRead = fread(inputVector, sizeof(char), sizeof(inputVector), inputFile);
+        if (bytesRead == 0) break;
+        MD5_Update(&hashChunk, inputVector, bytesRead);
+    }
+    MD5_Final(md5Vector, &hashChunk);
 
-  /* Czastka hasha */
-  MD5_CTX hashChunk;
-  MD5_Init(&hashChunk);
-
-  /* Obliczanie hasha */
-  size_t bytesRead;
-  while(1){
-    bytesRead=fread(inputVector, sizeof(char), sizeof(inputVector), inputFile);
-    if (bytesRead==0) break;
-    MD5_Update(&hashChunk, inputVector, bytesRead);
-  }
-  MD5_Final(md5Vector, &hashChunk);  
-
-  return md5Vector;
+    return md5Vector;
 }
 
-int main(int argc, char* argv[]){
-	  if (argc!=4){
-	    fprintf(stderr, "Uzycie programu: verify\n nazwaPlikuKlucza nazwaPlikuDoWeryfikacji plikPodpisu\n");
-	    return 1;
-	  }	
- 
+int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        fprintf(stderr, "Uzycie programu: verify\n nazwaPlikuKlucza nazwaPlikuDoWeryfikacji plikPodpisu\n");
+        return 1;
+    }
 
-	char* keyFileName = argv[1];
-	char* toVerifyFileName = argv[2];
-	char* signFileName = argv[3];
-	FILE* keyFile;
-	FILE* signFile;
-	RSA* rsaPublicKey;
-	unsigned char* md5Vector;
-	unsigned int signLength;
-	/* do wczytywania klucza */
-	long keySize;
-	unsigned char* buffer;
-	unsigned char* buffer2;
-	
-	/* wczytywanie klucza publicznego */
-	rsaPublicKey=RSA_new();
-	if ((keyFile=fopen(keyFileName,"rb"))==NULL){
-		fprintf(stderr, "Nie powios³o siê otwieranie pliku klucza publicznego\n");
-		return 1;
-	}
-	
-	fseek(keyFile,0,SEEK_END);
-	keySize=ftell(keyFile);
-	rewind(keyFile);
-	
-	buffer=(unsigned char*)malloc(keySize*sizeof(unsigned char));
-	buffer2=buffer; // jesli probuje bezposrednio to Naruszenie Ochrony Pamieci
-	fread(buffer,sizeof(unsigned char),keySize,keyFile);
-	d2i_RSAPublicKey(&rsaPublicKey,(const unsigned char**)&buffer2,keySize); //d2i_RSAPublicKey(**kluczRSA,**bufforZKluczemPKCS,dlugoscKluczaPKCS)
-	fclose(keyFile);
-	free(buffer);
-	
-	if (rsaPublicKey==NULL){
-		fprintf(stderr, "Nie powiod³o siê wczytywanie klucza publicznego\n");
-		return 1;
-	}
-	
-	md5Vector=md5sum(toVerifyFileName);
-	
-	if ((signFile=fopen(signFileName,"rb"))==NULL){
-		fprintf(stderr, "Nie udalo sie otworzyc pliku z podpisem\n");
-		return 1;
-	}
-	
-	fseek(signFile,0,SEEK_END);
-	signLength=ftell(signFile);
-	rewind(signFile);
-	
-	buffer=(unsigned char*)malloc(signLength*sizeof(unsigned char));
-	fread(buffer,sizeof(unsigned char),signLength,signFile);
-	fclose(signFile);
-	
-	if ((RSA_verify(NID_md5,md5Vector,sizeof(md5Vector),buffer,signLength,rsaPublicKey))==1){ //RSA_verify(tryb NID_nazwafunkcjihashujacej, *tablicaZHashem, dlugoscTablicyZHashem, *podpis, dlugoscPodpisu, *kluczRSA) // return 1 - zgadza sie 0 - nie zgadza sie
-		printf("Podpis jest autentyczny\n");
-	} 
-	else{
-		printf("Podpis jest sfa³szowany\n");
-	}
-	
-	free(md5Vector);
-	free(buffer);
-	RSA_free(rsaPublicKey);
-	
-	return 0;
+
+    char *keyFileName = argv[1];
+    char *toVerifyFileName = argv[2];
+    char *signFileName = argv[3];
+    FILE *keyFile;
+    FILE *signFile;
+    RSA *rsaPublicKey;
+    unsigned char *md5Vector;
+    unsigned int signLength;
+    /* do wczytywania klucza */
+    long keySize;
+    unsigned char *buffer;
+    unsigned char *buffer2;
+
+    /* wczytywanie klucza publicznego */
+    rsaPublicKey = RSA_new();
+    if ((keyFile = fopen(keyFileName, "rb")) == NULL) {
+        fprintf(stderr, "cannot open keyFileName\n");
+        return 1;
+    }
+
+    fseek(keyFile, 0, SEEK_END);
+    keySize = ftell(keyFile);
+    rewind(keyFile);
+
+    buffer = (unsigned char *) malloc(keySize * sizeof(unsigned char));
+    buffer2 = buffer; // jesli probuje bezposrednio to Naruszenie Ochrony Pamieci
+    fread(buffer, sizeof(unsigned char), keySize, keyFile);
+    d2i_RSAPublicKey(&rsaPublicKey, (const unsigned char **) &buffer2, keySize);
+    fclose(keyFile);
+    free(buffer);
+
+    if (rsaPublicKey == NULL) {
+        fprintf(stderr, "Cannot read rsaPublicKey\n");
+        return 1;
+    }
+
+    md5Vector = md5sum(toVerifyFileName);
+
+    if ((signFile = fopen(signFileName, "rb")) == NULL) {
+        fprintf(stderr, "Cannot open file signFileName\n");
+        return 1;
+    }
+
+    fseek(signFile, 0, SEEK_END);
+    signLength = ftell(signFile);
+    rewind(signFile);
+
+    buffer = (unsigned char *) malloc(signLength * sizeof(unsigned char));
+    fread(buffer, sizeof(unsigned char), signLength, signFile);
+    fclose(signFile);
+
+    if ((RSA_verify(NID_md5, md5Vector, sizeof(md5Vector), buffer, signLength, rsaPublicKey)) == 1) {
+        printf("Sign is true\n");
+    } else {
+        printf("Sign is false\n");
+    }
+
+    free(md5Vector);
+    free(buffer);
+    RSA_free(rsaPublicKey);
+
+    return 0;
 
 }
