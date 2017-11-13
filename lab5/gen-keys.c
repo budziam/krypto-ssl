9c4fd8b37ff0cf4c40051f5c47838433b4418358
Michał Budziak <michal.mariusz.b@gmail.com>
2017-11-13 16:30:42 +0100

    Refactor

--- lab5/gen-keys.c
+++ lab5/gen-keys.c
@@ -7,24 +7,26 @@
 #include <openssl/bn.h>
 
 
-int main(int argc, char* argv[]) {
-    if (argc<3){
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
         fprintf(stderr, "\n genKeys privateKeyName publicKeyName keyLength\n");
         return 1;
     }
-    char* privateKeyFileName = argv[1];
-    char* publicKeyFileName = argv[2];
-    char* keyLengthParameter = argv[3];
 
-    RSA* key;
-    FILE* out;
-    FILE* keyInfo = fopen("key-information.txt","wb");
+    char *privateKeyFileName = argv[1];
+    char *publicKeyFileName = argv[2];
+    char *keyLengthParameter = argv[3];
 
-    int keyLength = atoi(keyLengthParameter); //Below 1024 should be considered insecure
-    int exp = 3; // The exponent of e is an odd number, typically 3, 17 or 65537.
+    RSA *key;
+    FILE *out;
+    FILE *keyInfo = fopen("key-details", "wb");
+
+    int keyLength = atoi(keyLengthParameter);
+    int exp = 3;
     int pkcsLength;
-    unsigned char* buffer1;
-    unsigned char* buffer2;
+    unsigned char *buffer1;
+    unsigned char *buffer2;
+
     key = RSA_generate_key(keyLength, exp, NULL, NULL);
     fprintf(keyInfo, "PUBLIC MODULUS: ");
     BN_print_fp(keyInfo, key->n);
@@ -38,35 +40,37 @@ int main(int argc, char* argv[]) {
     BN_print_fp(keyInfo, key->q);
     fflush(keyInfo);
     fclose(keyInfo);
+
     if (RSA_check_key(key) != 1) {
         fprintf(stderr, "Please try again\n");
         return 1;
     }
 
-    if ((out=fopen(publicKeyFileName,"wb"))==NULL){
+    if ((out = fopen(publicKeyFileName, "wb")) == NULL) {
         fprintf(stderr, "Can't open public key file\n");
         return 1;
     }
 
-    pkcsLength = i2d_RSAPublicKey(key,NULL);
-    buffer1=(unsigned char*)malloc(pkcsLength*sizeof(unsigned char));
-    buffer2=buffer1;
+    pkcsLength = i2d_RSAPublicKey(key, NULL);
+    buffer1 = (unsigned char *) malloc(pkcsLength * sizeof(unsigned char));
+    buffer2 = buffer1;
 
-    i2d_RSAPublicKey(key,&buffer2);
+    i2d_RSAPublicKey(key, &buffer2);
 
-    fwrite(buffer1,sizeof(unsigned char),pkcsLength, out);
+    fwrite(buffer1, sizeof(unsigned char), pkcsLength, out);
     fflush(out);
     fclose(out);
     free(buffer1);
 
-    if ((out=fopen(privateKeyFileName, "wb"))==NULL){
+    if ((out = fopen(privateKeyFileName, "wb")) == NULL) {
         fprintf(stderr, "Can't open private key file\n");
         return 1;
     }
-    pkcsLength=i2d_RSAPrivateKey(key,NULL);
-    buffer1=(unsigned char*)malloc(pkcsLength*sizeof(unsigned char));
-    buffer2=buffer1;
-    i2d_RSAPrivateKey(key,&buffer2);
+
+    pkcsLength = i2d_RSAPrivateKey(key, NULL);
+    buffer1 = (unsigned char *) malloc(pkcsLength * sizeof(unsigned char));
+    buffer2 = buffer1;
+    i2d_RSAPrivateKey(key, &buffer2);
 
     fwrite(buffer1, sizeof(unsigned char), pkcsLength, out);
     fflush(out);
